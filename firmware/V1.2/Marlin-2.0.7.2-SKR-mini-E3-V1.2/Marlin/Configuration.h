ca6acd3700afcbe71969a01a69aed0b190e11434
Chris <chris.mah01@gmail.com>
2021-01-16 16:50:54 -0500

    invert e0 direction for new extruder

--- firmware/V1.2/Marlin-2.0.7.2-SKR-mini-E3-V1.2/Marlin/Configuration.h
+++ firmware/V1.2/Marlin-2.0.7.2-SKR-mini-E3-V1.2/Marlin/Configuration.h
@@ -981,7 +981,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -48, -9, -1 }
+#define NOZZLE_TO_PROBE_OFFSET { -47, -7, -1.52 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1088,7 +1088,7 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR true
+#define INVERT_E0_DIR false
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
